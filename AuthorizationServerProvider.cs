a41e587290f7012b2d9c161626073cd53df2ebcd
SahirG <35290662+SahirG@users.noreply.github.com>
2020-03-18 06:17:32 +0000

    Update AuthorizationServerProvider.cs

--- AuthorizationServerProvider.cs
+++ AuthorizationServerProvider.cs
@@ -72,6 +72,7 @@ namespace SmartAdminMvc
 				else
 				{
 					context.SetError("invalid_grant", "Provided username and password is incorrect");
+					string a = "bit bucket";
 					return;
 				}
 
