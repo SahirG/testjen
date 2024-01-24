10f922f27416608e5b0be8849db0b2bef1f93e69
SahirG <35290662+SahirG@users.noreply.github.com>
2024-01-24 18:29:59 +0530

    Update About.aspx.designer.cs

--- About.aspx.designer.cs
+++ About.aspx.designer.cs
@@ -13,7 +13,7 @@ namespace WebApplication1
 
     public partial class About
     {
-        string a = "Hello12123567";
-        string b = "Test123456789";
+        string a = "Hello";
+        string b = "Test";
     }
 }
